8e39b6240422720303b931fe2104f8a9003d9186
rhol <rhol@student.codam.nl>
2022-05-23 17:00:08 +0200

    split stack uses the new struct with lowest two, but second low not implemented yet

--- srcs/handle_less.c
+++ srcs/handle_less.c
@@ -1,29 +1,29 @@
 #include "push_swap.h"
 
-static int	get_lowest(t_list *head, int *low)
-{
-	t_list	*copy;
-	int		lowest_nbr;
-	int		lowest_loc;
-	int		i;
+// static int	get_lowest(t_list *head, int *low)
+// {
+// 	t_list	*copy;
+// 	int		lowest_nbr;
+// 	int		lowest_loc;
+// 	int		i;
 
-	copy = head;
-	lowest_nbr = INT_MAX;
-	lowest_loc = 0;
-	i = 0;
-	while (copy)
-	{
-		if (copy->nbr < lowest_nbr)
-		{
-			lowest_nbr = copy->nbr;
-			lowest_loc = i;
-		}
-		i++;
-		copy = copy->next;
-	}
-	*low = lowest_nbr;
-	return (lowest_loc);
-}
+// 	copy = head;
+// 	lowest_nbr = INT_MAX;
+// 	lowest_loc = 0;
+// 	i = 0;
+// 	while (copy)
+// 	{
+// 		if (copy->nbr < lowest_nbr)
+// 		{
+// 			lowest_nbr = copy->nbr;
+// 			lowest_loc = i;
+// 		}
+// 		i++;
+// 		copy = copy->next;
+// 	}
+// 	*low = lowest_nbr;
+// 	return (lowest_loc);
+// }
 
 static void	get_lowest_two(t_list *head, t_low_info *data)
 {
@@ -49,36 +49,34 @@ static void	get_lowest_two(t_list *head, t_low_info *data)
 	}
 }
 
-static void	handle_two(t_list **head)
-{
-	int		one;
-	int		two;
 
-	one = (*(head))->nbr;
-	two = (*(head))->next->nbr;
-	if (one > two)
-		sa(head, true);
-	return ;
-}
 
 static void	split_stack(t_list **head_a, t_list **head_b)
 {
 	int	len_a;
-	int	low_loc;
-	int	current_low;
+	// int	low_loc;
+	// int	current_low;
+
+	t_low_info data;
+
+	// get_lowest_two(*(head_a), &data);
+	// printf("first low: %d (%d)\n", data.low, data.l_loc);
+	// printf("second low: %d (%d)\n", data.sec_low, data.sl_loc);
+	// print_stacks(*(head_a), *(head_b));
 
 	len_a = ft_lstsize(*(head_a));
 	while (len_a > 3)
 	{
-		low_loc = get_lowest((*(head_a)), &current_low);
-		if (low_loc <= (len_a / 2))
+		get_lowest_two(*(head_a), &data);
+		// low_loc = get_lowest((*(head_a)), &current_low);
+		if (data.l_loc <= (len_a / 2))
 		{
-			while ((*(head_a))->nbr != current_low)
+			while ((*(head_a))->nbr != data.low)
 				ra(head_a, true);
 		}
 		else
 		{
-			while ((*(head_a))->nbr != current_low)
+			while ((*(head_a))->nbr != data.low)
 				rra(head_a, true);
 		}
 		pb(head_a, head_b);
@@ -113,6 +111,18 @@ static void	handle_three(t_list **head)
 	}
 }
 
+static void	handle_two(t_list **head)
+{
+	int		one;
+	int		two;
+
+	one = (*(head))->nbr;
+	two = (*(head))->next->nbr;
+	if (one > two)
+		sa(head, true);
+	return ;
+}
+
 void	handle_less(t_list **head_a, t_list **head_b)
 {
 	int	len_a;
@@ -128,10 +138,4 @@ void	handle_less(t_list **head_a, t_list **head_b)
 	handle_three(head_a);
 	while (*(head_b))
 		pa(head_a, head_b);
-	// testing time;
-	t_low_info data;
-	get_lowest_two(*(head_a), &data);
-	printf("first low: %d (%d)\n", data.low, data.l_loc);
-	printf("second low: %d (%d)\n", data.sec_low, data.sl_loc);
-	print_stacks(*(head_a), *(head_b));
 }
