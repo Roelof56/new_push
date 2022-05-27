8fe284e9cb5da6efc4075b7f88b6af638a7194df
rhol <rhol@student.codam.nl>
2022-05-27 14:44:01 +0200

    add some helper print functions to improve the whole thing

--- srcs/radix_bit_sort.c
+++ srcs/radix_bit_sort.c
@@ -9,8 +9,7 @@ static int	flip_bits(int n)
 	return (new);
 }
 
-// n is the number - loc(ation) is wich bit to get (0 is right most bit 31 is left.).
-// returns 1 || 0 for that bit loc.
+// returns the vallue of a bit on location loc in the number n.
 static int get_bit(int n, int loc)
 {
 	int	i;
@@ -32,6 +31,37 @@ static void	bit_flip_negative(t_list *list) // remove the copy ? !
 	}
 }
 
+// helper function to print stack binary.
+static void print_binary(int n)
+{
+	int	i;
+	int	j;
+	
+	i = 31;
+	j = 0;
+	printf("%14d : ", n);
+	while (i >= 0)
+	{
+		if (j % 4 == 0)
+			printf(" ");
+		printf("%d", get_bit(n, i));
+		i--;
+		j++;
+	}
+	printf("\n");
+}
+
+// temp print function.
+static void print_stack_binary(t_list *head)
+{
+	while (head)
+	{
+		printf("sign: %4d ", head->sign);
+		print_binary(head->nbr);
+		head = head->next;
+	}
+}
+
 /*
 **	sort based on bit vallue left to right.
 */
@@ -42,14 +72,17 @@ void	radix_bit_sort(t_list **head_a, t_list **head_b)
 	int		len;
 
 	i = 0;
+
 	// print the incomming list.
-	// print_stack_binary(program->head_a);
+	printf("original stack: \n");
+	print_stack_binary(*(head_a));
 
 	// flip them bits
 	bit_flip_negative(*(head_a));
-
+	
 	//print after flip
-	// print_stack_binary(program->head_a);
+	printf("\nafter bit flip: \n");
+	print_stack_binary(*(head_a));
 
 	// sort some stuff.
 
@@ -67,10 +100,10 @@ void	radix_bit_sort(t_list **head_a, t_list **head_b)
 				ra(head_a, true);
 			j++;
 		}
-		// print_stacks(program);
+		print_stacks(*(head_a), *(head_b));
 		while (*(head_b))
 			pa(head_a, head_b);
-		// print_stacks(program);
+		print_stacks(*(head_a), *(head_b));
 		if (is_sorted(*(head_a)))
 			break ;
 		i++;
