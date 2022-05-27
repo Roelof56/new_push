6d63bd2182e5ca330304c69d6e3c823e1099e091
rhol <rhol@student.codam.nl>
2022-05-27 16:05:00 +0200

    nice but not short enough

--- srcs/radix_bit_sort.c
+++ srcs/radix_bit_sort.c
@@ -3,33 +3,41 @@
 // invert all bits. '~' operator is 'not'
 static int	flip_bits(int n)
 {
-	int new;
+	int	new;
 
 	new = ~n;
 	return (new);
 }
 
 // returns the vallue of a bit on location loc in the number n.
-static int get_bit(int n, int loc)
+static int	get_bit(int n, int loc)
 {
 	int	i;
-	
-	i = n>>loc&1;
+
+	i = n >> loc & 1;
 	return (i);
 }
 
-static void	bit_flip_negative(t_list *head)
+// loop argument list. when an negative int is found. invert all them bits.
+static t_bool	bit_flip_negative(t_list *head)
 {
+	t_bool	contains_negative;
+
+	contains_negative = false;
 	while (head)
 	{
 		if (head->sign == -1)
+		{
 			head->nbr = flip_bits(head->nbr);
+			contains_negative = true;
+		}
 		head = head->next;
 	}
+	return (contains_negative);
 }
 
 // helper function to print stack binary.
-static void print_binary(int n)
+static void	print_binary(int n)
 {
 	int	i;
 	int	j;
@@ -49,13 +57,33 @@ static void print_binary(int n)
 }
 
 // temp print function.
-static void print_stack_binary(t_list *head)
+static void	print_stack_binary(t_list *head)
 {
 	while (head)
 	{
 		printf("sign: %4d ", head->sign);
 		print_binary(head->nbr);
 		head = head->next;
+	}
+}
+
+static void	handle_subzero_ints(t_list **head_a, t_list **head_b, int len)
+{
+	int	i;
+
+	i = 0;
+	while (i < len && *(head_a))
+	{
+		if (get_bit((*(head_a))->nbr, 31) == 1)
+			pb(head_a, head_b);
+		else
+			ra(head_a, true);
+		i++;
+	}
+	while (*(head_b))
+	{
+		rrb(head_b, true);
+		pa(head_a, head_b);
 	}
 }
 
@@ -67,20 +95,10 @@ void	radix_bit_sort(t_list **head_a, t_list **head_b)
 	int		i;
 	int		j;
 	int		len;
+	t_bool	contains_negative;
 
 	i = 0;
-
-	// print the incomming list.
-	printf("original stack: \n");
-	print_stack_binary(*(head_a));
-
-	// flip them bits
-	bit_flip_negative(*(head_a));
-	
-	//print after flip
-	printf("\nafter bit flip: \n");
-	print_stack_binary(*(head_a));
-
+	contains_negative = bit_flip_negative(*(head_a));
 	len = ft_lstsize(*(head_a));
 	while (i < 31)
 	{
@@ -95,34 +113,15 @@ void	radix_bit_sort(t_list **head_a, t_list **head_b)
 				ra(head_a, true);
 			j++;
 		}
-		print_stacks(*(head_a), *(head_b));
 		while (*(head_b))
 			pa(head_a, head_b);
-		print_stacks(*(head_a), *(head_b));
 		if (is_sorted(*(head_a)))
 			break ;
 		i++;
 	}
-	//revert numbers back to original.
-	bit_flip_negative(*(head_a));
-	print_stacks(*(head_a), *(head_b));
-
-	//put negatives back in in reverse order.
-	i = 0;
-	// check if there are negatives to trigger this.
-	// no there are exta operations for bull.
-	while (i < len && *(head_a))
+	if (contains_negative == true)
 	{
-		if (get_bit((*(head_a))->nbr, 31) == 1)
-			pb(head_a, head_b);
-		else
-			ra(head_a, true);
-		i++;
-	}
-	
-	while (*(head_b))
-	{
-		rrb(head_b, true);
-		pa(head_a, head_b);
+		bit_flip_negative(*(head_a));
+		handle_subzero_ints(head_a, head_b, len);
 	}
 }
